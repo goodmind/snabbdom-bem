8a8c3a95c51f6dbf7ba211bf6a90c9a56d97cea6
goodmind <andwebar@gmail.com>
2016-11-01 13:52:14 +0500

    style(ClassNameBuilder): remove unnecessary semicolons (there are still some WIP)

--- src/ClassNameBuilder.ts
+++ src/ClassNameBuilder.ts
@@ -1,26 +1,26 @@
-import { B } from 'b_'; // TODO: optimize?
+import { B } from 'b_' // TODO: optimize?
 
 export default class ClassNameBuilder {
   b_: any
 
   constructor(options: any) {
-    this.b_ = B(options);
+    this.b_ = B(options)
   }
 
   stringify(block: any, elem: any, mods?: any, mixes?: any[], cls?: any) {
     return this.b_(block, elem, mods) +
       (mixes ? ' ' + mixes.map(mix => this.b_(mix.block || block, mix.elem, mix.mods)).join(' ') : '') +
-      (cls ? ' ' + cls : '');
+      (cls ? ' ' + cls : '')
   }
 
   joinMixes(mix1: any, mix2?: any): any {
     if (Array.isArray(mix1)) {
-      mix2 = mix1[1];
-      mix1 = mix1[0];
+      mix2 = mix1[1]
+      mix1 = mix1[0]
     }
 
     if (!mix1 && !mix2) {
-      return undefined;
+      return undefined
     }
 
     let mixes: any[] = [];
@@ -28,7 +28,7 @@ export default class ClassNameBuilder {
     mix1 && (mixes = [...mixes, ...mix1]);
     mix2 && (mixes = [...mixes, ...mix2]);
 
-    return mixes;
+    return mixes
   }
 
 }
