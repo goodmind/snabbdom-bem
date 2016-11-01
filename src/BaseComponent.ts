70019e78ae30ca879018945cc511ff77a1376eb4
goodmind <andwebar@gmail.com>
2016-11-01 15:36:59 +0500

    style(BaseComponent): add license

--- src/BaseComponent.ts
+++ src/BaseComponent.ts
@@ -1,58 +1,62 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ If a copy of the MPL was not distributed with this file,
+ You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import * as inherit from 'inherit'
 
 class Component extends Object {
-  constructor(public props: any) {
+  constructor (public props: any) {
     super()
   }
 }
 
 export default inherit(Component, {
-  __constructor(): void {
+  __constructor (): void {
     this.__base.apply(this, arguments)
     this.willInit(this.props)
   },
 
-  willInit(): void {
+  willInit (): void {
     return
   },
 
-  tag(): string {
+  tag (): string {
     return 'div'
   },
 
-  attrs(): null {
+  attrs (): null {
     return null
   },
 
-  mods(): null {
+  mods (): null {
     return null
   },
 
-  cls(): string {
+  cls (): string {
     return ''
   },
 
-  mix(): null {
+  mix (): null {
     return null
   },
 
-  render(_props: any, _children: any): any {
+  render (): any {
     const { props } = this
     const res = this.__render(
-        this.tag(props),
-        this.attrs(props),
-        this.block,
-        this.elem,
-        this.mods(props),
-        [props.mix, this.mix(props)],
-        this.cls(props),
-        this.content(props, props.children)
-      )
+      this.tag(props),
+      this.attrs(props),
+      this.block,
+      this.elem,
+      this.mods(props),
+      [props.mix, this.mix(props)],
+      this.cls(props),
+      this.content(props, props.children)
+    )
 
     return this.wrap ? this.wrap(res) : res
   },
 
-  content(_: any, children: any): any {
+  content (_: any, children: any): any {
     return children
   }
 })
