7dabd6f488750ee85ac0ca8bb56c67c883bce10e
goodmind <andwebar@gmail.com>
2016-11-01 15:37:20 +0500

    style(ClassNameBuilder): add license

--- src/ClassNameBuilder.ts
+++ src/ClassNameBuilder.ts
@@ -1,19 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ If a copy of the MPL was not distributed with this file,
+ You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import { B } from 'b_' // TODO: optimize?
 
 export default class ClassNameBuilder {
   b: any
 
-  constructor(options: any) {
+  constructor (options: any) {
     this.b = B(options)
   }
 
-  stringify(block: any, elem: any, mods?: any, mixes?: any[], cls?: any) {
+  stringify (block: any, elem: any, mods?: any, mixes?: any[], cls?: any) {
     return this.b(block, elem, mods) +
       (mixes ? ' ' + mixes.map(mix => this.b(mix.block || block, mix.elem, mix.mods)).join(' ') : '') +
       (cls ? ' ' + cls : '')
   }
 
-  joinMixes(mix1: any, mix2?: any): any {
+  joinMixes (mix1: any, mix2?: any): any {
     if (Array.isArray(mix1)) {
       mix2 = mix1[1]
       mix1 = mix1[0]
