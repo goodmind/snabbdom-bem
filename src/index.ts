288b930627f0b4c3b62f0b611115bf052b76fb44
goodmind <andwebar@gmail.com>
2016-11-01 13:50:52 +0500

    style(index): remove semicolons

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import decls from './decls';
-import BaseComponent from './BaseComponent';
-import ClassNameBuilder from './ClassNameBuilder';
-import simpleComponent from './simpleComponent';
+import decls from './decls'
+import BaseComponent from './BaseComponent'
+import ClassNameBuilder from './ClassNameBuilder'
+import simpleComponent from './simpleComponent'
 
 const defaultNaming = new ClassNameBuilder({
     elementSeparator : '-',
     modSeparator : '_',
     modValueSeparator : '_'
   }),
-  defaultDecls = decls({}, BaseComponent, defaultNaming);
+  defaultDecls = decls({}, BaseComponent, defaultNaming)
 
-export const { decl, declMod } = defaultDecls;
+export const { decl, declMod } = defaultDecls
 export { html } from './html'
-export default simpleComponent(defaultNaming);
+export default simpleComponent(defaultNaming)
