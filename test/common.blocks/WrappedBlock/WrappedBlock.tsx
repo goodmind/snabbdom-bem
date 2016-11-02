1b0893dd1b6e0de97fbb38fca72e1a87d4915d2c
goodmind <andwebar@gmail.com>
2016-11-02 19:49:05 +0500

    style(WrappedBlock): change double colons to colons

--- test/common.blocks/WrappedBlock/WrappedBlock.tsx
+++ test/common.blocks/WrappedBlock/WrappedBlock.tsx
@@ -2,12 +2,13 @@
  If a copy of the MPL was not distributed with this file,
  You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// tslint:disable-next-line:no-unused-variable
 const { html } = require('snabbdom-bem')
 import Bem, { decl } from 'snabbdom-bem'
 
 export default decl({
   block: 'WrappedBlock',
   wrap (wrappedBlock) {
-    return <Bem block="Wrapper">{wrappedBlock}</Bem>
+    return <Bem block='Wrapper'>{wrappedBlock}</Bem>
   }
 })
